01e0f750ce2df5e1b1e036ebdf5b4e28b785e3e7
mmakinen <mmakinen@student.hive.fi>
2023-01-30 16:58:33 +0200

    changed to source code

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mmakinen/scop
+# Build directory: /Users/mmakinen/scop/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
