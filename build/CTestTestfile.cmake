c2a5f708c7194ea939537af003fa1942aa15803a
mmakinen <mmakinen@student.hive.fi>
2023-01-30 17:12:19 +0200

    cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mmakinen/scop
-# Build directory: /Users/mmakinen/scop/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-#include <SFML/Window.hpp>
+#include "./SFML/include/SFML/Config.hpp"
+#include "./SFML/include/SFML/Main.hpp"
+#include "./SFML/include/SFML/System.hpp"
+#include "./SFML/include/SFML/Window.hpp"
+#include "./SFML/include/SFML/Graphics.hpp"
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
