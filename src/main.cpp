8c6f9be37bc943951885ac81ca9f042a62bc774e
Max Mäkinen <quantumhound@gmail.com>
2023-02-08 14:42:36 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,85 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 #include "../include/scop.hpp"
 
+struct	ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
 
+static ShaderProgramSource ParseShader(const std::string &filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else
+		{
+			ss[(int)type] << line << "\n";
+		}
+	}
+	return { ss[0].str(), ss[2].str()};
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string &source)
+{
+	unsigned int	id = glCreateShader(type);
+	const char		*src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int	result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length = 0;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char message[length];
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return (0);
+	}
+
+	return (id);
+}
+
+static unsigned int CreateShader(const std::string &vertexShader, const std::string &fragmentShader)
+{
+	unsigned int	program = glCreateProgram();
+	unsigned int	vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int	fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return (program);
+}
 
 int main()
 {
@@ -13,7 +91,8 @@ int main()
     window.setActive(true);
 
 	// initialize GLEW after creating a valid OpenGL rendering context. Automatically done by SFML in previous command
-	glewInit();
+	if(glewInit() != GLEW_OK)
+		std::cout << "Error!: glewInit not ok" << std::endl;
 
     // load resources, initialize the OpenGL states, ...
 
@@ -28,19 +107,40 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	ShaderProgramSource source = ParseShader("../resources/shaders/basic.shader");
+	//unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	std::cout << source.VertexSource << std::endl;
+	std::cout << source.FragmentSource << std::endl;
+	//unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	//glUseProgram(shader);
+
     // run the main loop
     bool running = true;
     while (running)
     {
-		glClear(GL_COLOR_BUFFER_BIT);
 
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw...
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // end the current frame (internally swaps the front and back buffers)
+        window.display();
+
         // handle events
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			{
+				running = false;
+				break;
+			}
+            else if (event.type == sf::Event::Closed)
             {
                 // end the program
                 running = false;
@@ -51,17 +151,10 @@ int main()
                 glViewport(0, 0, event.size.width, event.size.height);
             }
         }
-
-        // clear the buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // draw...
-
-        // end the current frame (internally swaps the front and back buffers)
-        window.display();
     }
 
     // release resources...
+	//glDeleteProgram(shader);
 	window.setActive(false);
 
     return (0);
